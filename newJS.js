4b2d9b1f41afa7b82b22fd869007420d829840dc
Argisht <100313895+Argisht25@users.noreply.github.com>
2022-05-28 20:56:42 +0400

    Update newJS.js

--- newJS.js
+++ newJS.js
@@ -1,10 +1,3 @@
-function solution_test(a, b) {
-  return (a, b);
-}
-function solution(year) {
-  return Math.ceil(year / 100);
-}
-solution();
 function solution2(inputString) {
   for (let i = 0; j = inputString.length - 1; i < inputString.length, i++, j--)
     return (inputString[i] == inputString[j])
@@ -15,10 +8,3 @@ function solution2(inputString) {
   }
 };
 let inputS = "aaabaaa";
-
-
-let numbers = [66, 21, 12, 45, 35]
-function sorts(a, b) {
-  return a - b
-};
-alert(numbers.sort(sorts));
